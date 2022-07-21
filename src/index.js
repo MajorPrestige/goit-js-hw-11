9018c43e761f0b512397309f06937cad31c86fe9
Denis Dashkevich <majorprestige99@gmail.com>
2022-07-21 19:38:15 +0300

    finish project

--- src/index.js
+++ src/index.js
@@ -77,12 +77,3 @@ function onLoadBtnClick() {
       console.log(err);
     });
 }
-
-const { height: cardHeight } = document
-  .querySelector('.gallery')
-  .firstElementChild.getBoundingClientRect();
-
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: 'smooth',
-});
